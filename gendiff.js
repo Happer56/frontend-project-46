4fd32f58a2cade89def2e8d3c4c1089b3d99ce8a
Ruslan Galeev <happer1337@yandex.ru>
2025-05-26 22:30:49 +0500

    add gen

---
+++ gendiff.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node 
+import { program } from 'commander';
+
+
+
+program
+    .description('Compares two configuration files and shows a difference.')
+    .version('1.0')
+    .argument('<filepath1>')
+    .argument('<filepath2>')
+    .option('-f, --format [type]', 'output format')
+    
+
+
+program.parse()
+
