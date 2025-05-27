db14e537a66df942f4f6058a11adf5c224ec3321
Ruslan Galeev <happer1337@yandex.ru>
2025-05-27 22:52:31 +0500

    add gitignore

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node 
 import { program } from 'commander';
+import  gendiff1  from '../index.js';
 
 
 
@@ -9,8 +10,8 @@ program
     .argument('<filepath1>')
     .argument('<filepath2>')
     .option('-f, --format [type]', 'output format')
-    
+      .action((filepath1, filepath2, options) => {
+    console.log(gendiff1(filepath1, filepath2, options.format));
+  });
 
-
-program.parse()
-
+program.parse();
